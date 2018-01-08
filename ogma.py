4094cf5c0c0efb3fd15aa2cbae9164badb0baed6
Nosferatu <a00208226@student.ait.ie>
2018-01-08 12:02:58 +0000

    0.4v Watson Died :(
    Button listener - Yes
    Voice Reconision - Yes
    text to Speech -Yes

--- ogma.py
+++ ogma.py
@@ -1,22 +1,45 @@
+from pynput.keyboard import Key
+from pynput import keyboard
 import speech_recognition as sr
-from tts_watson.TtsWatson import TtsWatson
-
-
-# initiaze Watson text to Speech
-ttsWatson = TtsWatson('oskik313@gmail.com', 'MamNaImieOskar313.', 'en-US_AllisonVoice')
-ttsWatson.play("Hello World")
-
+import pyttsx3
 
 # get audio from the microphone
-r = sr.Recognizer()
-with sr.Microphone() as source:
-    print("Speak:")
-    audio = r.listen(source)
-try:
-    print("You said " + r.recognize_google(audio))
-    command = r.recognize_google(audio)
-except sr.UnknownValueError:
-    print("Could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results; {0}".format(e))
+def voiceRecognizer():
+    # Initiaze TtoS
+    xengine = pyttsx3.init()
+    # Initiaze voice recording
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Speak:")
+        audio = r.listen(source)
+        xengine.say('One moment , sir. ')
+        xengine.runAndWait()
+    try:
+        command = r.recognize_google(audio)
+        print(command)
+        xengine.say('Hello '+ command)
+        xengine.runAndWait()
 
+    except sr.UnknownValueError:
+        print("Could not understand audio")
+        xengine.say('Could you repeat that?. ')
+        xengine.runAndWait()
+    except sr.RequestError as e:
+        print("Could not request results; {0}".format(e))
+
+# listen for button pressed
+def on_press(key):
+    try:
+        print('alphanumeric key {0} pressed'.format(key.char))
+    except AttributeError:
+        print('special key {0} pressed'.format(key))
+        print(key)
+        if key == Key.insert:
+            print("so far so good")
+            voiceRecognizer()
+
+
+#Key listener
+with keyboard.Listener(
+        on_press=on_press) as listener:
+    listener.join()
